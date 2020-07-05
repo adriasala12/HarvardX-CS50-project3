c89dc055ad6216a6a5c093d5225f6a333ce22308
Adrià Sala <adriasala124@gmail.com>
2020-07-05 12:15:40 +0200

    Added basic login functionality

--- orders/views.py
+++ orders/views.py
@@ -1,12 +1,33 @@
-from django.http import HttpResponse
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 from .models import Pizza
 
+
 # Create your views here.
+
 def index(request):
 
-    context = {
+	context = {
         "meals": Pizza.objects.all()
     }
 
-    return render(request, "index.html", context)
+	return render(request, "index.html", context)
+
+
+def login_view(request):
+
+    return render(request, "login.html")
+
+def login_request(request):
+
+    username = request.POST["username"]
+    password = request.POST["password"]
+    user = authenticate(request, username=username, password=password)
+
+    if user is not None:
+        login(request, user)
+        return HttpResponseRedirect(reverse("index"))
+    else:
+        return render(request, "login.html", {"message": "Invalid credentials."})
